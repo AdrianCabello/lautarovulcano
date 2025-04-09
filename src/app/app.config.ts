6f6da507f191099871ed2a763a2f4fdf7c42ff35
Tu Nuevo Nombre <adrian.necoh@gmail.com>
2025-04-09 12:38:26 -0300

    Fix: Implementación de solución con .nojekyll y hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimations(),
-    provideHttpClient(),
-    { provide: LocationStrategy, useClass: PathLocationStrategy }
+    provideHttpClient()
   ]
 };
