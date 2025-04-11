2c931447c4b929ec9d221ecec539c529c125bc6c
Tu Nuevo Nombre <adrian.necoh@gmail.com>
2025-04-11 13:51:00 -0300

    refactor: remove routing and simplify to single page layout

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient(),
     provideClientHydration(), provideClientHydration(withEventReplay())
